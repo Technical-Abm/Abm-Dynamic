f54d3d6e5553aa40c9f8a5c4ecb6e2ba8abdad49
Abdul Basit Minhas <47935105+Technical-Abm@users.noreply.github.com>
2022-05-15 23:51:36 +0500

    Permanent deleted

--- dynamic.py
+++ dynamic.py
@@ -1,6 +1,6 @@
 import os
 os.system('clear')
-x = '\tScript has been deleted and abm onwer can not more update it'
+x = '\tScript has been permanent deleted and abm owner can not more update it'
 y = '\tThanks for using abm tool'
 print(x)
 print(y)
