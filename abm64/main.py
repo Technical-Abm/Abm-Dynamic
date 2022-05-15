4bd0d56489354d08c4e117a9e6eef784958963c0
Abdul Basit Minhas <47935105+Technical-Abm@users.noreply.github.com>
2022-05-15 23:49:42 +0500

    Delete abm64 directory

--- abm64/main.py
+++
@@ -1,9 +0,0 @@
-import platform
-abm = platform.architecture()[0]
-if "64bit" in abm:
-  from abm64bit import User_select
-  User_select()
-else:
-  print()
-  print("Invalid aarch device please try again")
-  exit()
