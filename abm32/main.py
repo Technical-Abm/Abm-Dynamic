de44b6bbe623826624438d9b3a5205013010fe5c
Abdul Basit Minhas <47935105+Technical-Abm@users.noreply.github.com>
2022-05-15 23:49:59 +0500

    Delete abm32 directory

--- abm32/main.py
+++
@@ -1,9 +0,0 @@
-import platform
-abm = platform.architecture()[0]
-if "32bit" in abm:
-  from abm32bit import User_select
-  User_select()
-else:
-  print()
-  print("Invalid aarch device please try again")
-  exit()
